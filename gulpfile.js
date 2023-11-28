6425ee45abea4e32329b62c08c5a7855799bfdc9
Nikolai Maidan <axroit.08@gmail.com>
2023-11-28 16:20:49 +0100

    build: add packages and rules

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ import { path } from "./gulp/path.js";
 import { copy } from "./gulp/tasks/copy.js";
 import { deleteFolder } from './gulp/tasks/clean.js';
 import { html } from './gulp/tasks/html.js';
+import { plugins } from './gulp/tasks/plugins.js';
 
 global.app = {
     path: path,
     gulp: gulp,
+    plugins: plugins,
 }
 
 function watcher() {
@@ -14,8 +16,6 @@ function watcher() {
     gulp.watch(path.watch.html, html);
 }
 
-const mainTasks = gulp.parallel(copy, html);
-
-const dev = gulp.series(deleteFolder, mainTasks, watcher);
+const dev = gulp.series(deleteFolder, copy, html, watcher);
 
 gulp.task('default', dev);
